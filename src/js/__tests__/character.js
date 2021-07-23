473fcd2783cfc7a8325c11feaa5b6c6da9ac1168
Dmitry Diakov <d.diakov@list.ru>
2021-07-23 09:31:56 +0500

    rename test filenames

--- src/js/__tests__/character.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable linebreak-style */
-import Bowman from '../Bowman';
-import Character from '../Character';
-
-test('new Character to throw', () => {
-  expect(() => new Character(1, 'bowman')).toThrow();
-  expect(() => new Bowman(1)).not.toThrow();
-});
